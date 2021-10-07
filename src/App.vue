a6bf81f463f2678ca0ec70638fbbff0c910fd9b8
Caio.Prado <caio100xa@gmail.com>
2021-10-07 20:36:22 -0300

    Adicionando vue router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <Home/>
+    <router-view path="$router.key" />
     <Footer/>
 
   </div>
@@ -11,12 +11,10 @@
 import './styles/global.css'
 
 import Footer from './components/Footer'
-import Home from './pages/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    Home,
     Footer,
   }
 }
